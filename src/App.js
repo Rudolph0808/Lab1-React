eadac853cbd55a7e16511afedeea31d3c395fd0a
Rodolfo Vega Dominguez <150092673+Rudolph0808@users.noreply.github.com>
2025-08-21 23:27:03 -0600

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import HolaMundo from "./HolaMundo";
+import Variables from "./Variables";
+import Airports from "./Airports";
+
+function App() {
+  return (
+    <div>
+      <HolaMundo />
+      <Variables />
+      <Airports />
+    </div>
+  );
+}
+
+export default App;
